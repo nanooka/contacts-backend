303fbc251ad1a236a66effacef7889d2e6c5588d
nanooka <102428722+nanooka@users.noreply.github.com>
2023-09-23 18:17:50 +0400

    now

--- routes.js
+++ routes.js
@@ -8,24 +8,13 @@ const cors = require("cors");
 app.use(cors());
 
 const corsOptions = {
-  origin: "http://localhost:3001",
+  // origin: "http://localhost:3001",
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-//
-
-// mongoose.connect("mongodb://localhost:27017/contacts", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected to MongoDB");
-// });
 
 app.post("/add_contact", async (request, response) => {
   const contact = new contactModel(request.body);
@@ -46,7 +35,3 @@ app.get("/contacts", async (request, response) => {
   }
 });
 module.exports = app;
-
-app.listen(3001, () => {
-  console.log("Server is running at port 3001");
-});
